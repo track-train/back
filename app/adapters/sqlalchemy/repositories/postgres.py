87cb6b52a5d27d175238461e283ad425805244ed
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2025-06-27 12:16:26 +0200

    add .gitignore, use uvlock and pyproject.toml add base create for bdd

--- app/adapters/sqlalchemy/repositories/postgres.py
+++ app/adapters/sqlalchemy/repositories/postgres.py
@@ -4,14 +4,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-db_url = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
-    db_url
+    DATABASE_URL,
+    echo=True,      
+    pool_size=10,   
+    max_overflow=20
 )
+Base.metadata.create_all(bind=engine)
+
 Session = sessionmaker(bind=engine)
 
-Base.metadata.create_all(bind=engine)
 
 def get_session():
     return Session()
--- app/entrypoints/api/main.py
+++ app/entrypoints/api/main.py
@@ -1,4 +1,15 @@
+import os
 from fastapi import FastAPI
+from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+from app.adapters.sqlalchemy.models import Base
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, echo=True)
 
 # on crée l'instance ASGI nommée "app"
 app = FastAPI(
@@ -6,6 +17,11 @@ app = FastAPI(
     description="Exemple d'API hexagonale avec FastAPI",
     version="0.1.0",
 )
+
+@app.on_event("startup")
+async def startup_event():
+    Base.metadata.create_all(bind=engine)
+
 
 @app.get("/")
 async def read_root():
