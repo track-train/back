01f694d0f10bea50a5601f536097d5b3406365aa
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2025-07-09 12:18:43 +0200

    create containerPY

---
+++ src/container.py
@@ -0,0 +1,25 @@
+import os
+from src.adapters.sqlalchemy.repositories.profil import SqlAlchemyProfileRepository
+from src.adapters.sqlalchemy.db import SessionLocal
+from src.domain.lib.security import BcryptPasswordHasher
+from src.domain.services.profil import ProfileService
+
+class Container:
+    def __init__(self, env: str | None = None):
+        self.env = os.getenv("ENV", "dev")
+        self.hasher = BcryptPasswordHasher()
+
+        self.SessionFactory = SessionLocal
+    
+    def get_profile_service(self):
+        if self.env == "dev":
+            # repo = InMemoryProfileRepository()
+            pass
+        else:
+            session= self.SessionFactory()
+            repo = SqlAlchemyProfileRepository(session)
+
+        return ProfileService(repo, self.hasher)
+
+
+container = Container()
