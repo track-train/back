a4d2554ef46d9907951e37ca1d75d1ec74abd622
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2025-07-11 18:22:09 +0200

    add new router exercice

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware import Middleware
 
 from src.entrypoints.api.routers.profile import router as profile_router
 from src.entrypoints.api.routers.group import router as group_router
+from src.entrypoints.api.routers.exercise import router as exercise_router
 
 
 app = FastAPI(
@@ -13,6 +14,7 @@ app = FastAPI(
 
 app.include_router(profile_router)
 app.include_router(group_router)
+app.include_router(exercise_router)
 
 
 
