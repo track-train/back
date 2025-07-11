e977e5190046cfd08ae7d64283fcf811efb9a4ed
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2025-07-11 13:12:27 +0200

    update container for new group context

--- src/container.py
+++ src/container.py
@@ -1,7 +1,9 @@
 import os
 from src.adapters.sqlalchemy.repositories.profile import SqlAlchemyProfileRepository
+from src.adapters.sqlalchemy.repositories.group import SqlAlchemyGroupRepository
 from src.adapters.sqlalchemy.db import SessionLocal
 from src.domain.lib.security import BcryptPasswordHasher
+from src.domain.services.group import GroupService
 from src.domain.services.profile import ProfileService
 
 class Container:
@@ -18,8 +20,19 @@ class Container:
         else:
             session= self.SessionFactory()
             repo = SqlAlchemyProfileRepository(session)
-
         return ProfileService(repo, self.hasher)
+        
+    
+    def get_group_service(self):
+        if self.env == "dev":
+            # repo = InMemoryGroupRepository()
+            pass
+        else:
+            session = self.SessionFactory()
+            repo = SqlAlchemyGroupRepository(session)
+        return GroupService(repo)
+
+
 
 
 container = Container()
