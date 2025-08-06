987f3f951f85b29d73d87cb8aec21b79f8f7cb9a
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2025-08-06 15:59:39 +0200

    re create feature whit test and all

--- src/adapters/inmemory/repositories/group.py
+++ src/adapters/inmemory/repositories/group.py
@@ -65,3 +65,11 @@ class InMemoryGroupRepository(GroupRepository):
 
     def find_all_groups(self) -> Optional[List[DomainGroup]]:
         return list(self._groups.values())
+
+    def find_groups_by_member_id(self, user_id: UUID) -> Optional[List[DomainGroup]]:
+        """Find all groups where the user is a member"""
+        groups = []
+        for group_id, member_ids in self._members.items():
+            if user_id in member_ids:
+                groups.append(self._groups[group_id])
+        return groups if groups else []
